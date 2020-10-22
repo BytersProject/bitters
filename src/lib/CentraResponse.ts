2637d73e6d3ff6214aef16c87d86e851b7e14d28
QuantumlyTangled <ethoarx@gmail.com>
2020-10-22 22:28:36 +0200

    feat: create response class

---
+++ src/lib/CentraResponse.ts
@@ -0,0 +1,38 @@
+import { RequestOptions, IncomingMessage } from 'http';
+
+export class CentraResponse {
+
+	public coreRes: IncomingMessage;
+	public resOptions: RequestOptions;
+	public body: Buffer;
+	public headers: IncomingMessage['headers'];
+	public statusCode: IncomingMessage['statusCode'];
+
+
+	public constructor(res: IncomingMessage, resOptions: RequestOptions) {
+		this.coreRes = res;
+		this.resOptions = resOptions;
+
+		this.body = Buffer.alloc(0);
+
+		this.headers = res.headers;
+		this.statusCode = res.statusCode;
+	}
+
+	// eslint-disable-next-line @typescript-eslint/naming-convention
+	public _addChunk(chunk: Buffer): void {
+		this.body = Buffer.concat([this.body, chunk]);
+	}
+
+	public json(): unknown {
+		return this.statusCode === 204 ? null : JSON.parse(this.body.toString());
+	}
+
+	public text(): string {
+		return this.body.toString();
+	}
+
+	public static parse: (text: string, ...args: any[]) => any = JSON.parse;
+
+}
+
