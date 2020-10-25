e877953b349f6a223fb478c6fcec2d209ad9fc48
QuantumlyTangled <ethoarx@gmail.com>
2020-10-25 14:29:46 +0100

    feat: add request building

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+import { RequestOptions } from 'http';
+import { URL } from 'url';
+import { BittersRequest } from './lib/BittersRequest';
+import { HttpMethods } from './lib/Enums';
+
 export * from './lib/BittersRequest';
 export * from './lib/BittersResponse';
 export * from './lib/Enums';
+
+export default (url: string | URL, method?: HttpMethods, coreOptions?: RequestOptions) => new BittersRequest(url, method, coreOptions);
